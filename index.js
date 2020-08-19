70fe31361d5cb666d2ecd7199ec79cd7aef036b5
Dmitrii Art <artuhovdmitrii@gmail.com>
2020-08-19 22:08:23 +0300

    before heroku deploy

--- index.js
+++ index.js
@@ -79,9 +79,15 @@ io.on('connection', (socket) => {
 
 // Server
 const PORT = process.env.PORT || config.PORT;
-// app.listen(PORT, () => {
-	// console.log(`Server started on port: ${PORT}`);
-// });
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('./client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
 http.listen(PORT, () => {
 	console.log(`Server started on port: ${PORT}`);
 });
